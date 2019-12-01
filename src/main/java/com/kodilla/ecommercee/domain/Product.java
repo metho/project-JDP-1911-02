e02cd528c9884da90d8a43515c1ff7d75853fecd
Mani3rrr <lukaszmajcher1@gmail.com>
2019-12-01 12:36:58 +0100

    Entity Product

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,11 +1,16 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+@Setter
+@Getter
 @Entity
 public class Product {
 
