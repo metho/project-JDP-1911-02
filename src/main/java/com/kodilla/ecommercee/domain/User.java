476b81c48c5af03f79cf14b0ece87f92d4f19cf1
pastorek89 <pastor.patryk@gmail.com>
2019-12-04 20:17:07 +0100

    JDP191102-25 conflicts resolved

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,17 +1,21 @@
 package com.kodilla.ecommercee.domain;
 
-import lombok.AllArgsConstructor;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class User {
@@ -19,11 +23,18 @@ public class User {
     @Id
     @GeneratedValue
     private Long id;
-    private String firstname;
-    private String lastname;
+    private String username;
+    private String password;
     private String address;
     private Integer postcode;
     private boolean blocked;
+
+    public User(String username, String password, String address, Integer postcode) {
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.postcode = postcode;
+    }
 
     @OneToOne
     @JoinColumn(name = "cart_id")
