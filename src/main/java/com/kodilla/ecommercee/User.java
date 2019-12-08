fd2f6195a9a6d67cd0583f259b2ea00af3de2b64
karoldd9 <karoldd9@gmail.com>
2019-12-08 10:23:26 +0100

    Merge JDP191102-3

--- src/main/java/com/kodilla/ecommercee/User.java
+++ src/main/java/com/kodilla/ecommercee/User.java
@@ -1,16 +1,11 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.Cart;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
