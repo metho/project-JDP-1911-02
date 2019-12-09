68eb7206f7862aad7f1a99ccf63624638a916cec
Mani3rrr <52953736+Mani3rrr@users.noreply.github.com>
2019-12-09 20:35:16 +0100

    Jdp191102 12 (#8)
    * Entity Product
    
    * Product Changes
    
    * Product Changes v2
    
    * New commit

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,13 +1,18 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
-
-
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import javax.persistence.Column;
 
+@AllArgsConstructor
+@Setter
 @Getter
 @Entity
 public class Product {
--- src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/ProductMapper.java
@@ -1,8 +1,30 @@
 package com.kodilla.ecommercee.mapper;
 
+import com.kodilla.ecommercee.domain.Product;
+import com.kodilla.ecommercee.domain.ProductDto;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class ProductMapper {
 
+    public Product mapToProduct(final ProductDto productDto) {
+        return new Product(productDto.getId(),
+                productDto.getName(),
+                productDto.getPrice());
+    }
+
+    public ProductDto mapToProductDto(final Product product) {
+        return new ProductDto(product.getId(),
+                product.getName(),
+                product.getPrice());
+    }
+
+    public List<ProductDto> mapToProductDtoList(final List<Product> productList) {
+        return productList.stream()
+                .map(this::mapToProductDto)
+                .collect(Collectors.toList());
+    }
 }
