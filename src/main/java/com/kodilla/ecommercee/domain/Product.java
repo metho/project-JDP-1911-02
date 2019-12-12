c51ff5f72ca2f8773b7b7cab316c336cc6deaa7a
Mani3rrr <lukaszmajcher1@gmail.com>
2019-12-12 19:55:33 +0100

    Dodanie relacji z GroupEntity

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -2,15 +2,15 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import javax.persistence.Column;
+import java.util.ArrayList;
+import java.util.List;
 
+@NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
@@ -27,4 +27,13 @@ public class Product {
     @NotNull
     @Column
     private int price;
+
+    @ManyToMany
+    @JoinTable(
+            name = "Join_Product_ProductGroup",
+            joinColumns = @JoinColumn(name = "PRODUCT_ID"),
+            inverseJoinColumns = @JoinColumn(name = "GROUPENTITY_ID")
+    )
+    private List<GroupEntity> productGroups = new ArrayList<>();
+
 }
